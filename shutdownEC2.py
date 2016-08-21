49efd3f29c5c3ee912a623ef990fe0c0838cb45b
hackstoic <hackstoic@163.com>
2016-08-21 21:10:47 +0800

    add two script for lambda service

---
+++ shutdownEC2.py
@@ -0,0 +1,30 @@
+import boto3
+import logging
+
+#setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# AWS credentials
+ACCESS_KEY = "AKIAxxxxxxxxxexample"
+SECRET_KEY = "qae6xxxxxxxxxexample"
+
+#define the connection
+ec2 = boto3.client(
+    'ec2',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+)
+
+InstanceIds=[
+        'your_instance_id',
+]
+
+def lambda_handler(event, context):
+    response = ec2.stop_instances(
+        DryRun=False,
+        InstanceIds=InstanceIds,
+        Force=False
+    )
+    logger.info(response)
+    return InstanceIds
